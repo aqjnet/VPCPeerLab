400344bc4ddb9f1675b51f9202a148947a3ef94a
Andrew <andrewquinnjaques@gmail.com>
2024-08-21 16:38:24 +0100

    working 1 ec2 - public with ssh

--- main.tf
+++ main.tf
@@ -1,31 +1,29 @@
-## INSTANCES #################
+####EC2 INSTANCE####
 
-resource "aws_instance" "PC1" {
-  ami           = var.ami
+resource "aws_instance" "lesson_05" {
+  ami           = "ami-097a2df4ac947655f"
   instance_type = "t2.micro"
+  key_name      = "aws_key"
+  subnet_id     = aws_subnet.my_subnet.id
   vpc_security_group_ids = [
     aws_security_group.sg_ssh.id,
     aws_security_group.sg_https.id,
     aws_security_group.sg_http.id
   ]
-  subnet_id = aws_subnet.my_subnet.id
-  key_name  = "aws_key"
-
 
   tags = {
-    area           = "iDMZ"
-    classification = "internal"
+    Name                    = "Lesson_05"
+    Vanquisher_of_Gothmog   = "Ecthelion"
+    Vanquisher_of_Ecthelion = "Gothmog"
   }
 }
 
 resource "aws_key_pair" "deployer" {
   key_name   = "aws_key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDOZLL2yEPZmd5Et7uWAyCgySXI2ZduZYBbsd9Xqfn1xSTGJDjm8u9oYbfz3JObcwSPiNg858MTQbuJzRbnMMUgnMb4P6qTQbvljOjAO3Y737uBuWPKB93GTvsk0wSRYcEw6ljW/voH+7GxDf5y6r6K0D2y+a2z5FSMjODjcKhnqzmgufweQFtbUX1XLlJp8007q9+utQx1OH0YiPPyMCVNpHxR093haUPKIFlHyLx1WOTKx3Mu5kXT9NgPzjtzwp18Wm5pCK7CzepYnA5I09Iw1B8UWN4klEOhqPoi7kUMeP7OBOf1l1vMYgtw2oHe52vu5/s9XIJ/Eo3byxbhscRU0XGaI1A452E9aRP0HEfTkWZgks2zXRec9B2Pk28xdxEGKiBIkiSwbh/Uil8zVgUrQfdOT/Hodhk8UkAXFZH/4qfavQR7W3grbCdHtCItj9NRRrIeLwqEzi5K/kL6jTvtcyVII2QOZha0FqIFco5JcvChsT7BJDosRag7LAoebUM= admin@PC1"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7Uqs70iiGd4Uk2kvrLszQY3xDZnZ85ZkrytI7gDHkEOhYjvuvLMw3garg784Tn761m0tVVP8WmDkW46ds/FykuyBjsNswLJk/RNzOcPY6LIbFY01WZ7a9uXsOoGVY/x4cpURvGvNOCo5WcvapxqhtdFij7LXdgmD6hssePa+NpNbLBCCSuqC7MWlxybtJdYq9QBb7pnVRr5FGv2cCUEjaPA2JmSg2qYgjhBCYHe85AK5/x7f02d1FRjVtNJIK07c7X0H7FwND6uwApC5giJcV2QWnnNgbNu3R9IKlWW3nYai8nDZqArGqph8Jx8F3tgOX4TjE6dEMw619I6qShAxDbD3ThQBIUVbncHix5HFIxkDc8Do+7VvLAKAl963/GbQoJHs5O09B1PJtHwVE5bEcBvX4NV/1e873/gadw/HOnllNz4x9qmJqijk8B0R35kNAmnnJ1+yEvoP65uLLVs676BDu2WAStPlBsKLiP272ua9xeraTEWpI8LWgOLq0gb0= admin@PC1"
 }
 
-## NETWORKING ##
-
-##THE VPC##
+####VPC####
 
 resource "aws_vpc" "my_vpc1" {
   cidr_block = "172.16.0.0/16"
@@ -35,7 +33,46 @@ resource "aws_vpc" "my_vpc1" {
   }
 }
 
-#~SECURITY GROUPS FOR YOUR VPC~#
+####NETWORKING####
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc1.id
+  cidr_block              = "172.16.10.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_internet_gateway" "prod-igw" {
+  vpc_id = aws_vpc.my_vpc1.id
+  tags = {
+    Name = "prod-igw"
+  }
+}
+
+resource "aws_route_table" "prod-public-crt" {
+  vpc_id = aws_vpc.my_vpc1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.prod-igw.id
+  }
+
+  tags = {
+    Name = "prod-public-crt"
+  }
+}
+resource "aws_route_table_association" "prod-crta-public-subnet-1" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.prod-public-crt.id
+}
+
+
+####SECURITY GROUPS####
+
 resource "aws_security_group" "sg_ssh" {
   vpc_id = aws_vpc.my_vpc1.id
 
@@ -55,10 +92,10 @@ resource "aws_security_group" "sg_ssh" {
 }
 
 resource "aws_security_group" "sg_https" {
-
   vpc_id = aws_vpc.my_vpc1.id
+
   ingress {
-    cidr_blocks = ["192.168.0.0/16"]
+    cidr_blocks = ["172.16.0.0/16"]
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
@@ -88,41 +125,4 @@ resource "aws_security_group" "sg_http" {
     from_port   = 0
     to_port     = 0
   }
-}
-
-#SUBNET - REMEMBER IT NEEDS A ROUTE OUT OF THE SUBNET!!!################################
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc1.id
-  cidr_block              = "172.16.10.0/24"
-  availability_zone       = "eu-west-2a"
-  map_public_ip_on_launch = "true"
-
-  tags = {
-    Name = "tf-example"
-  }
-}
-
-resource "aws_internet_gateway" "prod-igw" {
-    vpc_id = aws_vpc.my_vpc1.id
-    tags = {
-        Name = "prod-igw"
-    }
-}
-
-resource "aws_route_table" "prod-public-crt" {
-    vpc_id = aws_vpc.my_vpc1.id
-    
-    route {
-        cidr_block = "0.0.0.0/0" 
-        gateway_id = aws_internet_gateway.prod-igw.id
-    }
-    
-    tags = {
-        Name = "prod-public-crt"
-    }
-}
-resource "aws_route_table_association" "prod-crta-public-subnet-1"{
-    subnet_id = aws_subnet.my_subnet.id
-    route_table_id = aws_route_table.prod-public-crt.id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "ec2_instance_public_ip" {
   description = "Private IP addresses of the EC2 instances"
-  value       = aws_instance.PC1.public_ip
+  value       = aws_instance.lesson_05.public_ip
 }
